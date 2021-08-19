229f8ba3fde7c4f5ac874d2d3f98fadb6fcc21c3
Laurentiu Barbulescu <lrbarbulescu@gmail.com>
2021-08-19 12:38:46 -0700

    Initial commit

---
+++ content/en/docs/sessions/05-contributing-to-unikraft/work/02-task-porting/src/libs/kdtree/include/test_suite_glue.h
@@ -0,0 +1,9 @@
+#ifndef __TEST_SUITE_GLUE__LIBKDTREE_H__
+#define __TEST_SUITE_GLUE__LIBKDTREE_H__
+
+int test1_main(int argc, char *argv[]);
+int test2_main(int argc, char *argv[]);
+
+int libkdtree_test_main();
+
+#endif /* __TEST_SUITE_GLUE__LIBKDTREE_H__ */
